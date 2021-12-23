e5d4f9d05c274c380a6bad313ebcd132b8f006bd
Quo210 <gabrielrengifo211@gmail.com>
2021-12-23 00:14:21 -0400

    added small functions module, added buttons and operators creators and appended on doc

--- src/html-elements.js
+++ src/html-elements.js
@@ -5,7 +5,7 @@ export const htmlElements = (()=>{
         for (let i = 1;i < 10; i++){
             const newButton = document.createElement('button');
             newButton.classList.add('number');
-            newButton.setAttribute('data-key',i.toString());
+            newButton.setAttribute('data-key',`${i}`);
             newButton.textContent = i;
             buttons.push(newButton);
         }
@@ -13,17 +13,43 @@ export const htmlElements = (()=>{
         const zero = document.createElement('button');
         zero.classList.add('number');
         zero.setAttribute('data-key','0');
+        zero.textContent = '0';
         const dot = document.createElement('button');
         dot.classList.add('number');
         dot.setAttribute('data-key','.');
+        dot.textContent = '.' 
 
         buttons.push(zero,dot);
 
-        return buttons;
+        return Array.from(buttons);
+    }
+
+    function operatorButtons(){
+        const buttons = makeButtons(7,'operator');
+        const arr = ['+','-','*','/','DEL','C','='];
+        const dataKeys = ['add','subs','mult','div','del','clear','equals']
+        for (let i = 0; i < buttons.length; i++){
+            buttons[i].textContent = arr[i];
+            buttons[i].setAttribute('data-key',`${dataKeys[i]}`)
+        }
+        
+        return buttons
+    }
+
+    function makeButtons(quant,clazz){
+        const buttons = [];
+        for (let i = 0; i<quant; i++){
+            const newButton = document.createElement('button');
+            newButton.classList.add(clazz);
+            buttons.push(newButton)
+        }
+
+        return Array.from(buttons);
     }
 
     return {
-        a: numbButtons,
+        mkNums: numbButtons,
+        mkOper: operatorButtons,
     }
 
 })()
--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,17 @@
 import './style.css';
 import { htmlElements } from './html-elements';
+import { myTools } from './tools';
 
-const alpha = htmlElements.a();
+const myModule = (()=>{//Mostly DOM and joining other modules
 
-console.log(alpha)
+const panel = document.querySelector('section.panel');
+const screen = document.querySelector('section.screen');
+
+const numbers = htmlElements.mkNums();
+const operators = htmlElements.mkOper();
+
+myTools.appendFromArr(panel,numbers)
+myTools.appendFromArr(screen,operators)
+
+
+})()
